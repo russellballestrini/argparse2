5ffef54c0fcdbd37b276f295d161d8aa231ede1c
russellballestrini <russell.ballestrini@gmail.com>
2017-06-06 23:28:31 -0400

    modified:   argparse2/__init__.py

--- argparse2/__init__.py
+++ argparse2/__init__.py
@@ -4,15 +4,13 @@ from argparse import ArgumentParser
 class EnvParser(ArgumentParser):
 
     def _get_env_var_name(self, flag_name):
-        """--port becomes PROG_PORT"""
-        return '{}_{}'.format(self.prog, flag_name.lstrip('-')).replace('-', '_').upper()
+        """port becomes PROG_PORT"""
+        return '{}_{}'.format(self.prog, flag_name).replace('-', '_').upper()
 
-    def add_argument(self, *args, **kwargs):
+    def add_argument(self, *args, **kw):
         flag_name = args[1].lstrip('-') if len(args) == 2 else args[0].lstrip('-')
         env_var = self._get_env_var_name(flag_name)
-        default = environ.get(env_var, kwargs.get('default', ''))
-        help_text = kwargs.get('help', '') + ', {}={}'.format(env_var, flag_name)
-        del kwargs['default']
-        del kwargs['help']
-        return super(EnvParser, self).add_argument(*args, default=default, help=help_text, **kwargs)
+        kw['default'] = environ.get(env_var, kw.get('default', ''))
+        kw['help'] = kw.get('help', '') + ', {}={}'.format(env_var, flag_name)
+        return super(EnvParser, self).add_argument(*args, **kw)
 
